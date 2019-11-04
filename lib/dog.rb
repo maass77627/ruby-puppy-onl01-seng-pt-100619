fa09ea26d4c277412d5e9e2741ada78c2e69395b
kelly r maass <kmaass77627@gmail.com>
2019-11-04 01:24:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Add your code here
+class Dog 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name) 
+    @name = name
+    @@all << self
+    @@print_all = "name"
+    end 
+    
+   def self.all 
+     @@all
+    end 
+    
+    def self.print_all 
+      @@all.each do |name|
+       #{name}
+    end 
+  end
+    
+  end 
